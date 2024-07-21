ea9194c0c2da4cd278764864e6a5743c88900772
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-07-21 21:28:08 +0530

    chore(webpack): :wrench: Add webpack configuration file

---
+++ webpack.config.mjs
@@ -0,0 +1,31 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import nodeExternals from 'webpack-node-externals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  target: 'node',
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts'],
+    alias: {
+      '@': path.resolve(__dirname, 'src/')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  externals: [nodeExternals()]
+};
