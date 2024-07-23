f00ce27a13ba93e17603564f616185a2ba40f3d0
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-07-24 00:06:26 +0530

    feat(user.schema): sparkles: Add new name field to user schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -1,11 +1,22 @@
-import { UserRole } from "@/types/enum.types";
-import { toTuple } from "@/utils/helpers";
-import zodSchemaGenerator from "@/utils/validators/ZodSchemaGenerator";
-import { CreateUserDTO } from "@/types/dtos/user.dto.types";
+import { UserRole } from '@/types/enum.types';
+import { toTuple } from '@/utils/helpers';
+import zodSchemaGenerator from '@/utils/validators/ZodSchemaGenerator';
+import { CreateUserDTO } from '@/types/dtos/user.dto.types';
 const z = zodSchemaGenerator.getValidatorObject();
 
 export const createUserSchema =
   zodSchemaGenerator.generateSchema<CreateUserDTO>({
+    name: z
+      .string()
+      .regex(/^[A-Za-z][A-Za-z' -]*$/, 'Invalid name format')
+      .transform((name) =>
+        name
+          .split(' ')
+          .map(
+            (word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+          )
+          .join(' ')
+      ),
     email: z.string().email(),
-    role: z.enum(toTuple(Object.values(UserRole))),
+    role: z.enum(toTuple(Object.values(UserRole)))
   });
