af8f9b9726d299cd0993031cb36e7eec36564c22
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-07-24 00:12:19 +0530

    fix(auth.schema): :bug: Add missing properties to loginSchema

--- src/types/dtos/auth.dto.types.ts
+++ src/types/dtos/auth.dto.types.ts
@@ -1,12 +1,12 @@
-import { IUserAttrs, IUserDocument } from "@/types/models/user.model.types";
-import { DTOFactory } from "@/types/utility.types";
+import { IUserAttrs, IUserDocument } from '@/types/models/user.model.types';
+import { DTOFactory } from '@/types/utility.types';
 
 export type LoginDTO = DTOFactory<
   IUserDocument,
-  keyof Omit<IUserAttrs, "role">
+  keyof Omit<IUserAttrs, 'name' | 'role'>
 >;
 
 export type ResetPasswordDTO = DTOFactory<
   IUserDocument,
-  keyof Pick<IUserDocument, "email" | "password" | "role" | "_confirmPassword">
+  keyof Pick<IUserDocument, 'email' | 'password' | 'role' | '_confirmPassword'>
 >;
