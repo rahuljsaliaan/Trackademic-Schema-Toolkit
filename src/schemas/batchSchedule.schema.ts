2100cc7bd005842b8234da4519a24f2bcc29200c
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-07-28 16:25:32 +0530

    enhance(batchSchedule): :star: Improve batchSchedule schema and model types

--- src/schemas/batchSchedule.schema.ts
+++ src/schemas/batchSchedule.schema.ts
@@ -3,17 +3,44 @@ import ZodSchemaGenerator from '@/utils/validators/ZodSchemaGenerator';
 
 const z = ZodSchemaGenerator.getValidatorObject();
 
-export const batchScheduleSchema =
+const timeSchema = z
+  .string()
+  .regex(/^([01]\d|2[0-3]):([0-5]\d)$/, 'Invalid time format. Expected HH:MM');
+
+const scheduleSchema = z
+  .object({
+    order: z.number().positive(),
+    startTime: timeSchema,
+    endTime: timeSchema
+  })
+  .refine(
+    (data) => {
+      const [startHour, startMinute] = data.startTime.split(':').map(Number);
+      const [endHour, endMinute] = data.endTime.split(':').map(Number);
+      return (
+        (endHour ?? 0) > (startHour ?? 0) ||
+        ((endHour ?? 0) === startHour && (endMinute ?? 0) > (startMinute ?? 0))
+      );
+    },
+    {
+      message: 'endTime must be greater than starTime',
+      path: ['endTime']
+    }
+  );
+
+export const createBatchScheduleSchema =
   ZodSchemaGenerator.generateSchema<CreateBatchScheduleDTO>({
-    schedule: z.array(
-      z.object({
-        subject: z.string(),
-        time: z
-          .string()
-          .regex(
-            /^([01]\d|2[0-3]):([0-5]\d)$/,
-            'Invalid time format. Expected HH:MM'
-          )
-      })
+    schedule: z.record(z.string(), scheduleSchema).refine(
+      (schedules) => {
+        const orders = Object.values(schedules).map(
+          (schedule) => schedule.order
+        );
+        return orders.every((order, index) => order === index + 1);
+      },
+      {
+        message:
+          'Order must be incremental and based on the position within the map',
+        path: ['schedule']
+      }
     )
   });
--- src/types/models/batchSchedule.model.types.ts
+++ src/types/models/batchSchedule.model.types.ts
@@ -3,17 +3,23 @@ import { IBatchDocument } from '@/types/models/batch.model.types';
 
 export interface IBatchScheduleAttrs {
   batch: string;
-  schedule: { subject: string; time: string }[];
+  schedule: Record<
+    string,
+    { order: number; startTime: string; endTime: string }
+  >;
 }
 
 export interface IBatchScheduleDocument extends Document<IBatchScheduleAttrs> {
   id: string;
   batch: string | IBatchDocument;
   /**
-   * A map where the key is the subject and the value is the time.
-   * @type {Map<string, string>}
+   * A Record where the key is the subject and the value is the time.
+   * @type {Record<string, { starTime: string; endTime: string }> }
    */
-  schedule: { subject: string; time: string }[];
+  schedule: Record<
+    string,
+    { order: number; startTime: string; endTime: string }
+  >;
   createdAt: Date;
   updatedAt: Date;
   isDeleted: boolean;
