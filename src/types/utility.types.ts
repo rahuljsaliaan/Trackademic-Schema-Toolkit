ba4c4ecd31c8ce331dcdb57d47cf7cc72e606f87
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-08-10 14:35:42 +0530

    remove(utility.types): :fire: Remove unused mongoose ObjectId type

--- src/types/utility.types.ts
+++ src/types/utility.types.ts
@@ -1,13 +1,10 @@
-import { Document, Types } from 'mongoose';
+import { Document } from 'mongoose';
 
 /** Creates a type by picking a subset of properties from T. */
 export type DTOFactory<T extends Document, K extends keyof T> = Pick<T, K>;
 
 /** Extracts the keys of type T as a union of literal types. */
 export type AllKeys<T> = keyof T;
-
-/** Type definition for a Mongoose ObjectId. */
-export type MongooseObjectId = Types.ObjectId;
 
 /**
  * Utility type to recursively omit properties from nested objects.
