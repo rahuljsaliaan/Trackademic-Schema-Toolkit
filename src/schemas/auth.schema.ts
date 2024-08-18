77e7a409b3e8d5cab3a6c72ac9611948cd0bf99f
rahuljsaliaan <rahuljsaliaan@gmail.com>
2024-08-19 00:25:03 +0530

    enhance(attendance.schema): :star: Add otp to the schema

--- src/schemas/auth.schema.ts
+++ src/schemas/auth.schema.ts
@@ -45,9 +45,13 @@ export const resetPasswordSchema =
   });
 
 export const verifyOTPSchema = ZodSchemaGenerator.generateSchema<VerifyOTPDTO>({
-  email: emailSchema,
+  email: z.string().email(),
   otp: z
-    .number()
-    .min(TrackademicSchemaConfig.Auth.OTP_LENGTH)
-    .max(TrackademicSchemaConfig.Auth.OTP_LENGTH)
+    .string()
+    .min(TrackademicSchemaConfig.Auth.OTP_LENGTH, {
+      message: `OTP must be at least ${TrackademicSchemaConfig.Auth.OTP_LENGTH} characters long`
+    })
+    .max(TrackademicSchemaConfig.Auth.OTP_LENGTH, {
+      message: `OTP must be at most ${TrackademicSchemaConfig.Auth.OTP_LENGTH} characters long`
+    })
 });
